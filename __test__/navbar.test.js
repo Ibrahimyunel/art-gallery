d5d23591d7ce34f22c797ad882d12ff51d6282a8
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-03-11 14:34:37 +0700

    add NavResponsive class

--- __test__/navbar.test.js
+++ __test__/navbar.test.js
@@ -1,9 +1,14 @@
-import { Navbar } from '../js/navbar.js';
+import { Navbar, NavResponsive } from '../js/navbar.js';
 
 describe('test class', () => {
     const navbarObj = new Navbar();
-    test('it should read as element', () => {
-        expect(typeof navbarObj.navbar).toBeTruthy();
+    test('it should be object', () => {
+        expect(typeof navbarObj.navbar).toBe('object');
+    });
+
+    const navbarResObj = new NavResponsive();
+    test('it should be function', () => {
+        expect(typeof navbarResObj.onResize).toBe('function');
     });
 });
 
--- js/navbar.js
+++ js/navbar.js
@@ -15,3 +15,42 @@ export class Navbar {
         window.addEventListener('scroll', this.navScroll);
     }
 }
+
+export class NavResponsive {
+    constructor() {
+        this.navbarList = document.getElementById("navbar_list");
+        this.openMenu = document.getElementById("open_menu");
+        this.closeMenu = document.getElementById("close_menu");
+    }
+
+    resNav = (e) => {
+        if (e.target.id.includes('open_menu')) {
+            this.navbarList.classList.add('navbar-res');
+            this.openMenu.style.display = 'none';
+            this.closeMenu.style.display = 'block';
+        } else {
+            this.navbarList.classList.remove('navbar-res');
+            this.openMenu.style.display = 'block';
+            this.closeMenu.style.display = 'none';
+        }
+    }
+
+    windowResize = () => {
+        console.log(window.innerWidth);
+        if (window.innerWidth >= 770) {
+            this.openMenu.style.display = 'none';
+            if (this.closeMenu.style.display === 'block') {
+                this.navbarList.classList.remove('navbar-res');
+                this.closeMenu.style.display = 'none';
+            }
+        } else {
+            this.openMenu.style.display = 'block';
+        }
+    }
+
+    onResize() {
+        this.openMenu.addEventListener('click', this.resNav);
+        this.closeMenu.addEventListener('click', this.resNav);
+        window.addEventListener('resize', this.windowResize)
+    }
+}
